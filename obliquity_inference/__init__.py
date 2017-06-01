fa9079d9c0828cb86fd1183c4e82b07d0bbce7a5
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-06-01 15:58:06 -0700

    Basics for plotting routines

--- obliquity_inference/__init__.py
+++ obliquity_inference/__init__.py
@@ -10,6 +10,7 @@ __all__ = ['cosi_pdf','cosi_pdf_interp',
            'compute_equatorial_velocity_dataframe',
            'compute_inclination_dataframe',
            'generate_orientation_sample',
+           'sample_distribution',
            'hellinger_distance',
            'total_variation_distance',
            'plotting'
@@ -24,5 +25,6 @@ from inclination_posterior import compute_cosipdf_from_dataframe,\
     compute_inclination_dataframe,\
     posterior_cosi_full, posterior_cosi_analytic, sample_veq_vals,\
     compute_equatorial_velocity_dataframe
-from inclination_distribution import generate_orientation_sample
+from inclination_distribution import generate_orientation_sample, \
+    sample_distribution
 from significance import hellinger_distance, total_variation_distance
